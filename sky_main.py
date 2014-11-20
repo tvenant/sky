7b029c9d41a64779eb0c6fbb8ee24ad9f9f3b391
tvenant <ventum11@yahoo.fr>
2014-11-20 11:54:13 +0200

    adding response, integrating twitter bootstrap and error files

--- sky_main.py
+++ sky_main.py
@@ -1,6 +1,6 @@
 __author__ = 'john'
 
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template, make_response
 from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -21,10 +21,25 @@ def greeting(name2):
 def counter(value):
     values = {0:'Zero', 1:'One', 2:'Two', 3:'Three', 4:'Four', 5:'Five'}
     return render_template('counter.html',value = values.get(value,'unkown'))
+
 @app.route('/browser')
 def which_browser():
+    # import pdb;pdb.set_trace()
     user_agent = request.headers.get('User-Agent')
-    return render_template('browser.html',browser=user_agent)
+    response = render_template('browser.html',browser=user_agent)
+    return response
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
+
+@app.route('/response')
+def my_response():
+    return make_response("<h1>Hello my sweet Response!</h1>")
 @app.route('/test')
 def testing():
     pass
