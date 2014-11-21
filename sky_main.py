21c0cfed98481a741257656233d91d6354ac4239
tvenant <ventum11@yahoo.fr>
2014-11-21 09:09:38 +0200

    adding form, alert message

--- sky_main.py
+++ sky_main.py
@@ -1,17 +1,40 @@
 __author__ = 'john'
 
-from flask import Flask,request,render_template, make_response
+from flask import Flask,request,render_template, make_response,redirect, session, url_for,flash
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '-_=)+=@"<.>?/C54(*7`~``~N,3290<N..the_DANGROUS!|"$#%1Q__from__KIGALI!|!|!'
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',current_time = datetime.utcnow())
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html',name = name)
+class NameForm(Form):
+    name = StringField('What is your name?', validators = [Required()])
+    submit = SubmitField()
+
+@app.route('/user',methods=['GET','POST'])
+def user():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('user'))
+    return render_template('user.html',form=form, name = session.get('name'))
 
 @app.route('/greeting/<name2>')
 def greeting(name2):
@@ -40,10 +63,15 @@ def internal_server_error(e):
 @app.route('/response')
 def my_response():
     return make_response("<h1>Hello my sweet Response!</h1>")
+
+@app.route('/facebook')
+def redirect_to_facebook():
+    return redirect('http://www.facebook.com')
+
 @app.route('/test')
 def testing():
     pass
 
-bootstrap = Bootstrap(app)
+
 if __name__ == '__main__':
     app.run(debug = True)
